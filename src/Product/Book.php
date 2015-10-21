e96ae9232fa1b6325936723a542b596d8c45a8f1
Maxim Zhurilo <fumus.27@gmail.com>
2015-10-21 21:06:11 +0300

    commited new class Book

---
+++ src/Product/Book.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fumus
+ * Date: 21.10.15
+ * Time: 20:35
+ */
+namespace Product;
+
+use Price\PriceInterface;
+
+class Book extends AbstractProduct implements PriceInterface
+{
+    protected $isbn;
+    protected $author;
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function setDiscount($discount)
+    {
+        $this->discount = $discount;
+    }
+
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+
+    public function getPrice()
+    {
+        $pr = $this->price;
+
+        if ($this->discount) {
+            $pr = $pr - ($this->discount)*$pr/100;
+        }
+
+        return $pr;
+    }
+
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+    }
+
+    public function getIsbn()
+    {
+        return $this->isbn;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+}
