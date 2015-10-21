1d96341bf5e2c4d7ffe6bdde256a764f25defc17
Maxim Zhurilo <fumus.27@gmail.com>
2015-10-21 20:10:38 +0300

    created an Apparel object instance

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: fumus
- * Date: 21.10.15
- * Time: 17:19
- */
+
+require_once 'vendor/autoload.php';
+
+use Product\Apparel;
+
+$instance = new Apparel();
+$instance->setProductName('Pant');
+$instance->setProducerCompany('Nike');
+$instance->setPrice(650);
+$instance->setDiscount(25);
+$instance->setColor('blue');
+$instance->setSize('M');
+$instance->setMaterial('Cotton');
+
+var_dump($instance);
+echo $instance->getPrice();
