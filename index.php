64a6df02b612f81c58d4f2556a02b0c3fcfe27a4
Maxim Zhurilo <fumus.27@gmail.com>
2015-10-21 21:07:21 +0300

    commited changes in index.php

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 require_once 'vendor/autoload.php';
 
 use Product\Apparel;
+use Product\Book;
 
 $instance = new Apparel();
 $instance->setProductName('Pant');
 $instance->setProducerCompany('Nike');
 $instance->setPrice(650);
+// Set discount in dollars
 $instance->setDiscount(25);
 $instance->setColor('blue');
 $instance->setSize('M');
@@ -15,3 +17,15 @@ $instance->setMaterial('Cotton');
 
 var_dump($instance);
 echo $instance->getPrice();
+
+$book = new Book();
+$book->setProductName('Assembler in examples');
+$book->setProducerCompany('Computer Press');
+$book->setAuthor('Rudolf Marek');
+$book->setIsbn('5-94387-232-9');
+$book->setPrice(100);
+// Set discount in percents
+$book->setDiscount(5);
+
+var_dump($book);
+echo $book->getPrice();
