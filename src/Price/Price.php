df6417dc26a3a3149998093c74346b9c299c7361
Maxim Zhurilo <fumus.27@gmail.com>
2015-10-21 18:48:10 +0300

    added file src/Price/Price.php

---
+++ src/Price/Price.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fumus
+ * Date: 21.10.15
+ * Time: 18:37
+ */
+namespace Price;
+
+interface PriceInterface
+{
+    public function getPrice();
+
+    public function setPrice($price);
+
+    public function setDiscount($discount);
+}
